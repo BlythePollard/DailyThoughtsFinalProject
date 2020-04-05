e1cc69f5d7e78524524e1cec94dfcc429b7caba8
BlythePollard <github email address>
2020-04-04 21:15:36 -0600

    added classes for day, reflections, observations, built in basic html insertion

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
    
-    def create
-        @user = User.find_by(name: params[:name])
-        if @user
-            session[:user_id] = @user.id
-            render json: @user
-        else 
-            @user = User.new(user_params)
-            if @user.save
-                session[:user_id] = @user.id
-                render json: @user
-            end
-        end
-    end
+#     def create
+#         @user = User.find_by(name: params[:name])
+#         if @user
+#             session[:user_id] = @user.id
+#             render json: @user
+#         else 
+#             @user = User.new(user_params)
+#             if @user.save
+#                 session[:user_id] = @user.id
+#                 render json: @user
+#             end
+#         end
+#     end
 
-    private
+#     private
 
-    def user_params
-        params.require(:note).permit(:name)
-    end
-end
+#     def user_params
+#         params.require(:note).permit(:name)
+#     end
+# end
